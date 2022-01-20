cfb0990e8837d438706b31ebf50199e8e10976fa
Pavel Remizov <remizov.p1@azurgames.com>
2022-01-21 00:10:38 +0300

    Add point class

---
+++ OOP_Homework/OOP_Homework/Point.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace OOP_Homework
+{
+    /*
+    Создать класс Point (точка) как потомок геометрической фигуры.
+    */
+    internal class Point: Figure
+    {
+        public Point(Color color, bool isVisible, System.Drawing.Point center) : base(color, isVisible, center)
+        { }
+    }
+}
